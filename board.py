e2e4ca541801d0d0c799935e1636d2485fab834f
Sydney Dicks <49503981+sydneydicks@users.noreply.github.com>
2020-02-24 21:33:49 +0000

    Initial class implementations
    So these are just the basic implementations of the player, die, and board classes; I took the stuff directly from our class diagram so it's super basic

---
+++ board.py
@@ -0,0 +1,10 @@
+import Die
+import Property
+
+class Board:
+
+    def __init__(self):
+        #holds array of Property objects in their positions on the board
+        tileArray = [][]
+        #instatiates the die for the beginning of the game
+        gameDie = Die()
---
+++ player.py
@@ -0,0 +1,19 @@
+import Property
+
+class Player:
+    #initialize all
+    def __init__(self):
+        #input from player
+        #name of token piece
+        self.tokenName = ""
+        self.ankBalance = 0
+        #position on board in regards to board array
+        self.position = [0,0]
+        #holds an array of Property objects
+        self.propertiesOwned = []
+        #holds an array of Property objects
+        self.propertiesMortgaged = []
+        self.hasWon = false
+        self.inJail = false
+        jailTimeCount = 0
+        
