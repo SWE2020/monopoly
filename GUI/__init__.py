3013da75086af9a4f355267186ccc3bc65d5d2b9
Ege <egeersu@gmail.com>
2020-04-18 23:30:02 +0300

    add card images

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -4,3 +4,4 @@ if __name__ == '__main__':
 else:
     from GUI.game_intro import game_intro
     from GUI.name_selection import select
+    import GUI.utils
--- PropertyTycoon.py
+++ PropertyTycoon.py
@@ -9,23 +9,24 @@ pygame.init()
 #pygame.mixer.music.play(-1)
 
 # Play the intro, save number of players
-num_players = GUI.game_intro()
-#num_players = 3
+#num_players = GUI.game_intro()
+num_players = 3
 
 # Player Name Selection
-Player_Names = GUI.select(num_players)
-#Player_Names = ["Ege", "Deniz", "Elif"]
+#Player_Names = GUI.select(num_players)
+Player_Names = ["Ege", "Kaleb", "Evan"]
 
 # display
 DISPLAY_SIZE = (1440,770); DISPLAY_COLOR = (110,110,110)
 gamedisplay = pygame.display.set_mode(DISPLAY_SIZE)
 pygame.display.set_caption("Property Tycoon")
-#background2 = pygame.image.load("GUI/background2.jpg")
-board_image = pygame.image.load("GUI/Board.jpg")
-prop = pygame.image.load("YFS.png")
+
+background2 = pygame.image.load("GUI/images/backgrounds/background2.jpg")
+board_image = pygame.image.load("GUI/images/board.jpg")
+prop = pygame.image.load("GUI/images/property_cards/YFS.png")
+
 run = True
 while run:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -34,8 +35,9 @@ while run:
             print("click")
 
     gamedisplay.fill(DISPLAY_COLOR)
+    gamedisplay.blit(background2, (0, 0))
     gamedisplay.blit(board_image, (0, 0))
-    gamedisplay.blit(rescale(prop, 0.20), (1130,0))
+    gamedisplay.blit(GUI.utils.rescale(prop, 0.20), (1130,0))
     pygame.display.update()
 
 
