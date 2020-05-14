c0ccfa2e7d44b0aa35e449dfa8fd5e913701978a
Ege Ersü <egeersu@gmail.com>
2020-05-15 00:21:42 +0300

    Update rescale.py

--- GUI/utils/rescale.py
+++ GUI/utils/rescale.py
@@ -1,5 +1,4 @@
 import pygame
-import numpy
 
 def rescale(img, scale):
     h1, w1 = img.get_size()
@@ -7,7 +6,4 @@ def rescale(img, scale):
     return img
 
 def grayscale(img):
-    arr = pygame.surfarray.array3d(img)
-    avgs = [[(r*0.298 + g*0.587 + b*0.114) for (r,g,b) in col] for col in arr]
-    arr = numpy.array([[[avg,avg,avg] for avg in col] for col in avgs])
-    return pygame.surfarray.make_surface(arr)
+    return 0
